39eeb732f8cfe67f8d63b5a188106d917ff37e73
Daniela Barros <105089871+ghdpb@users.noreply.github.com>
2025-09-22 16:27:50 -0300

    Client .NET para acessar ollama local

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using OllamaSharp;
+
+var url = new Uri("http://localhost:11434");
+var client = new OllamaApiClient(url);
+
+//var models = await client.ListLocalModelsAsync();
+
+//foreach (var model in models)
+    //Console.WriteLine(model.Name);
+
+client.SelectedModel = "phi3:latest";
+
+var chat = new Chat(client);
+string? prompt = Console.ReadLine() ?? string.Empty;
+
+await foreach (string answer in chat.SendAsync(prompt))
+    Console.WriteLine(answer);
+
+Console.WriteLine();    
+Console.WriteLine("--");
